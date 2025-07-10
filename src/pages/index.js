26c7fc675525a9f18c37672817abe71bde454462
Bhautik <bhautikrchudasama@gmail.com>
2025-07-10 20:53:40 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      Hello World.{" "}
+      Hello Bhautik.{" "}
       <Link href="/about">
         About
       </Link>
